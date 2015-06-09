761ef2fe80c20a49b254a89d7b4b54af4dc14d3e
Jake Greenstein <jag28020@gmail.com>
2015-06-08 20:22:49 -0400

    Update cart on backend

--- app/search.js
+++ app/search.js
@@ -57,6 +57,23 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http){
 		$scope.cart.items[item.id] = addItem;
 		console.log(JSON.stringify($scope.cart));
 
+		var json = JSON.stringify($scope.cart);
+		var url = '/api/carts/'+$scope.cart['id'];
+		var headers = {headers: {'Authorization': $scope.token}};
+		$http.put(url, json, headers).success(function(data, status, headers, config) {
+        	var results = data['results'];
+        	console.log(JSON.stringify(results));
+        	
+            if (results.confirmation != 'success'){
+                alert(results['message']);
+                return;
+            }
+			
+        }).error(function(data, status, headers, config){
+            console.log("error", data, status, headers, config);
+        });
+
+
 	}
 
 	$scope.search = function(isInitial){
