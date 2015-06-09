6811a0cc1bc5d0ff5ad90c815af5794121bd4309
Jake Greenstein <jag28020@gmail.com>
2015-06-08 20:43:37 -0400

    Try push item to array

--- app/search.js
+++ app/search.js
@@ -53,9 +53,9 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http){
 	$scope.addToCart = function(item, site, result){
 		console.log(JSON.stringify(item));
 		console.log(JSON.stringify(site));
-		var addItem = {'name':result.name, 'brand':result.brand, 'price':item.price, 'seller':item.seller, 'sku':site.sku, 'url':site.url};
+		var addItem = { item.id : {'name':result.name, 'brand':result.brand, 'price':item.price, 'seller':item.seller, 'sku':site.sku, 'url':site.url}};
 		console.log(JSON.stringify(addItem));
-		$scope.cart['items'].item.id = addItem;
+		$scope.cart['items'].push(addItem);
 		console.log(JSON.stringify($scope.cart));
 
 		// var json = JSON.stringify($scope.cart);
