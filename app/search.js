9ab7372c124aa927feffc71ed72286090012e726
Jake Greenstein <jag28020@gmail.com>
2015-06-08 20:05:28 -0400

    Added items to cart

--- app/search.js
+++ app/search.js
@@ -50,9 +50,13 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http){
 	        });
 		}
 
-	$scope.addToCart = function(item, site){
+	$scope.addToCart = function(item, site, result){
 		console.log(JSON.stringify(item));
 		console.log(JSON.stringify(site));
+		var addItem = {'name':result.name, 'brand':result.brand, 'price':item.price, 'seller':item.seller, 'sku':site.sku, 'url':site.url};
+		cart[item.id] = addItem;
+		console.log(JSON.stringify(cart));
+
 	}
 
 	$scope.search = function(isInitial){
