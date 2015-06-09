049f04ef226cccc7e2d8fbb81876e6f6674322c5
Jake Greenstein <jag28020@gmail.com>
2015-06-08 22:37:10 -0400

    changed . to bracket

--- app/search.js
+++ app/search.js
@@ -54,7 +54,7 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http){
 		// console.log(JSON.stringify(item));
 		// console.log(JSON.stringify(site));
 		var itemDetails = {'name':result.name, 'brand':result.brand, 'price':item.price, 'seller':item.seller, 'sku':site.sku, 'url':site.url};
-		var addItem = {item.id:itemDetails};
+		var addItem = {item['id']:itemDetails};
 		// console.log(JSON.stringify(addItem));
 		$scope.cart['items'].push(addItem);
 		console.log(JSON.stringify($scope.cart));
